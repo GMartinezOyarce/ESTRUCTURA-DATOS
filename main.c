00c4577f5fbd7cf55e5e1d7886b10a340a4721fb
martindiaz1 <diazvallemartinfelipe@gmail.com>
2025-05-17 19:47:05 -0400

    Struct Imputados

--- main.c
+++ main.c
@@ -10,12 +10,40 @@ struct Denuncia{
   char *origenDenunciante;
   /*Fecha en la que se hizo la denuncia*/
   char *fecha;
-  /*Si la denuncia sigue en proceso o ya termino,
-*   0 = Terminado  1 = Sigue vigente*/
-  int estado;
+  /*Estado de la denuncia*/
+  char *estado;
   /*Descripcion del Caso*/
   char *descripcion;
+  /*Si Imputado == NULL, no se ha identificado al imputado*/
+  struct Imputado *imputado;
+ }
+
+/*Lista Simplemente enlazada*/
+struct NodoDeclaraciones{
+  char *declaracion;
+  struct NodoDeclaraciones *sig;
 }
+/*Lista Circular Simplemente enlazada de imputados*/
+struct NodoImputados{
+  struct Imputado *imputado;
+  struct NodoImputados *sig;
+}
+
+struct Imputado{
+  /*Denuncia que se acuso al Imputado*/
+  struct Denuncia *denuncia;
+  /*Rut del imputado*/
+  char *rut;
+  /*Causa del cual se esta investigando al imputado*/
+  char *causa;
+  /*Medidas que se tomo para controlar al imputado*/
+  char *medidasCautelares;
+  /*Estado del caso del imputado*/
+  int estado;
+  /*Lista simplemente enlazada de declaraciones*/
+  struct NodoDeclaraciones *declaraciones;
+}
+
 
 int main(){
 
