34e27a307da61289e14f9c80576d3156531cf406
martindiaz1 <diazvallemartinfelipe@gmail.com>
2025-05-17 18:48:37 -0400

    Struct Denuncia

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+struct Denuncia{
+  /* RUC Para identificar la denuncia*/
+  char *ruc;
+  /* Rut del denunciante*/
+  char *denunciante;
+  /*Quien hizo la denuncia*/
+  char *origenDenunciante;
+  /*Fecha en la que se hizo la denuncia*/
+  int fecha;
+  /*Si la denuncia sigue en proceso o ya termino,
+*   0 = Terminado  1 = Sigue vigente*/
+  int estado;
+  /*Descripcion del Caso*/
+  char *descripcion;
+}
 
 int main(){
 
