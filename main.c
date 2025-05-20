2f41093f16220eddaafeece3919666887523413d
martindiaz1 <diazvallemartinfelipe@gmail.com>
2025-05-19 23:46:56 -0400

    Structs Ministerio, fiscales, peticiones y unos cambios al struct demanda

--- main.c
+++ main.c
@@ -10,12 +10,20 @@ struct Denuncia{
   char *origenDenunciante;
   /*Fecha en la que se hizo la denuncia*/
   char *fecha;
-  /*Estado de la denuncia*/
+  /*Razon por la que se esta denunciando*/
+  char *causa;
+  /*Estado de la denuncia
+   * * En investigacion, cerrada, en juicio o archivada
+   */
   char *estado;
   /*Descripcion del Caso*/
   char *descripcion;
-  /*Si Imputado == NULL, no se ha identificado al imputado*/
-  struct Imputado *imputado;
+
+  /*Si Imputado == NULL, no se ha identificado al posible responsable,
+   * una vez se apruebe la investigacion, el posible responsable pasa a ser imputado
+   */
+  int *rutPosibleResponsable;
+
  };
 
 /*Lista Simplemente enlazada*/
@@ -28,8 +36,6 @@ struct NodoImputados{
   struct Imputado *datosImputado;
   struct NodoImputados *sig;
 };
-
-
 
 
 
@@ -48,6 +54,47 @@ struct Imputado{
   struct NodoDeclaraciones *declaraciones;
 };
 
+struct Peticion {
+  char *ruc;
+  char *rut;
+  /*Quien pidio la peticion (Victima, abogados, pdi, carabineros u otros)*/
+  char *origenPeticion;
+  char *tipoSolicitud;
+  char *descripcionSolicitud;
+  /*Si aprobacion == 0, la peticion todavia no se ha revisado
+   * Si aprobacion == 1, la peticion se ha aprobado
+   * si aprobacion == -1, la peticion se ha rechazado
+   */
+  int aprobacion;
+};
+/*Lista Simple con nodo Fantasma*/
+struct NodoPeticiones {
+  struct Peticion *peticion;
+  struct NodoPeticiones *sig;
+};
+
+
+
+struct Fiscal {
+  int rut;
+  char *contrasenia;
+  /*Lista simplemente enlazada con nodo fantasma*/
+  struct NodoPeticiones *Peticiones;
+  /*lista simplemente enlazada, Posiblemente se cambie a circular doblemente enlazada,
+   * porque en este caso solo se agregan denuncias, no se eliminan
+   */
+  struct NodoDenuncias *Denuncias;
+  /*lista simplemente enlazada juicios*/
+  struct NodoJuicioOral *Juicios;
+  /*arbol binario con carpetas de investigacion*/
+  struct ArbolCarpetas *carpetas;
+};
+
+/*Struct principal con un arreglo fiscales de tamaño TamFiscal*/
+struct MinisterioPublico {
+  struct Fiscal **fiscales;
+  int tamFiscal;
+};
 
 int main(){
 
